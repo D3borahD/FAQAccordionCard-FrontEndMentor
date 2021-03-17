a337eb7d87490680ead2c91f7524dc83036c58e8
Deb <deborah.douillet@yahoo.fr>
2021-03-17 15:01:53 +0100

    premier test javascript

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-const arrow = document.querySelectorAll('.arrow');
+// Activation de la flèche UNE OK 
+//const arrow = document.querySelector('.arrow');
+
+// arrow.addEventListener('click', () => {
+// const ask = document.querySelector('.question');
+//     ask.classList.toggle('active');
+// })
+
+// Selection de l'ensemble des flèches
+
+// const arrow = document.querySelectorAll('.arrow');
+// const ask = document.querySelector('.question');
 
 // arrow.forEach((item)=> {
-//     item.addEventListener('click', (e) => {
-//         arrow.classList.toggle('active');
-
+//     item.addEventListener('click', () => {
+//     ask.classList.toggle('active');
+//     })
 // })
+
+// 
+
+
+
