63881c51b3424b49ccf60f20d19d0d258a5f08de
Deb <deborah.douillet@yahoo.fr>
2021-03-18 14:38:40 +0100

    résolution accordéion JS

--- main.js
+++ main.js
@@ -1,66 +1,8 @@
-// Activation de la flèche UNE OK 
+const accordion = document.getElementsByClassName("accordion");
+var i;
 
-// function fleche () {
-// const arrow = document.querySelector('.arrow');
-// arrow.addEventListener('click', () => {
-// const ask = document.querySelector('.question');
-//     ask.classList.toggle('active');
-// })
-// }
-
-
-
-// Selection de l'ensemble des flèches
-
-// function fleche () {
-
-//     const arrow = document.querySelectorAll('.arrow');
-//     const ask = document.querySelector('.question');
-
-//     arrow.forEach((item)=> {
-//         item.addEventListener('click', () => {
-//         ask.classList.toggle('active');
-//         })
-//     })
-// }
-
-// fleche();
-
-
-// ajouter une class à l'élément parent
-
-	// var accItem = document.getElementsByClassName('accordionItem');
-    // var accHD = document.getElementsByClassName('accordionItemHeading');
-
-    // for (i = 0; i < accHD.length; i++) {
-    //     accHD[i].addEventListener('click', toggleItem, false);
-    // }
-
-    // function toggleItem() {
-    //     var itemClass = this.parentNode.className;
-    //     for (i = 0; i < accItem.length; i++) {
-    //         accItem[i].className = 'accordionItem close';
-    //     }
-    //     if (itemClass == 'accordionItem close') {
-    //         this.parentNode.className = 'accordionItem open';
-    //     }
-    // }
-
-   // Source aide :  https://codepen.io/sureshrkm/pen/ZbzBpr
-//
-const question = document.getElementsByClassName('question');
-const arrow = document.getElementsByClassName('arrow');
-
-for (i = 0; i < arrow.length; i++){
-    arrow[i].addEventListener('click', toggleItem, false);
-}
-
-function toggleItem() {
-    const itemClass = this.parentNode.className;
-    for (i = 0; i < question.length; i++) {
-        question[i].className = ' question close';
-    }
-    if (itemClass == 'question close') {
-        this.parentNode.className = 'question open';
-    }
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle("active");
+    })
 }
